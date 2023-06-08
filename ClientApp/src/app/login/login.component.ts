999374940b87bb570d3f229ad783a3ff9335cb64
moralessIvann <42558181+moralessIvann@users.noreply.github.com>
2023-06-08 14:35:26 -0700

    added logic to login component

--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { UsuarioApiService } from '../services/usuarioApi.service';
 import { AuthenticationAPIJson } from '../modelos/authenticationAPIJson';
 import { environment } from '../../environments/environment';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { UsuarioAPIJson } from '../modelos/usuarioAPIJson';
+import { ClienteJson } from '../modelos/clienteJson';
 
 @Component({
   selector: 'app-login-component',
   templateUrl: './login.component.html'
 })
 
-export class LoginComponent implements OnInit
-{
+export class LoginComponent implements OnInit {
 
   usuarioAPI: AuthenticationAPIJson;
 
-  constructor(private servicioLogin: UsuarioApiService)
-  {
-    console.log("constr")
-    console.log("constr")
+  //form
+  loginForm: FormGroup;
+  enviado: boolean = false;
+  resultadoPeticion: string;
+  @ViewChild("myModalInfo", { static: false }) myModalInfo: TemplateRef<any>;
+
+  token: string;
+
+  constructor(private servicioLogin: UsuarioApiService, private formBuilder: FormBuilder,
+    private modalService: NgbModal) {
+
     // ServicioProducto.obtenerClientes().subscribe(res => { console.log(res)});
     this.usuarioAPI = {
       email: environment.usuarioAPI,
@@ -25,9 +35,42 @@ export class LoginComponent implements OnInit
   }
 
   ngOnInit() {
-    console.log("ngOnInit")
+
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      pass: ['', Validators.required]
+    })
+
+    // servicio para llamar login API 
     this.servicioLogin.loginAPI(this.usuarioAPI).subscribe(respuesta => {
+      if (respuesta.error != null && respuesta.error != '')
+        this.resultadoPeticion = respuesta.texto;
+      else
+        this.token = (respuesta.objetoGenerico as UsuarioAPIJson).token;
       console.log(respuesta)
     })
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.loginForm.controls;
+  }
+
+  Login() {
+    this.enviado = true;
+    if (this.loginForm.invalid) {
+      console.log("Invalido");
+      return;
+    }
+
+    let cliente: ClienteJson =
+    {
+      email: this.loginForm.controls['email'].value,
+      password: this.loginForm.controls['pass'].value
+    };
+
+
+    //Llamar método de Login incrustando el token en la cabecerea
+    this.resultadoPeticion = this.token;
+    this.modalService.open(this.myModalInfo);
   }
 }
---
+++ ClientApp/src/app/modelos/usuarioAPIJson.ts
@@ -0,0 +1,4 @@
+export interface UsuarioAPIJson {
+  email: string;
+  token: string;
+}
